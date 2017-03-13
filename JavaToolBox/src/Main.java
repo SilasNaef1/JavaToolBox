e365377bf122b433d3fdaac07d8078a2adb6afff
JannisThymnios <j.thymnios@gmail.com>
2017-03-13 10:21:51 +0100

    Header hinzugefügt

--- JavaToolBox/src/Main.java
+++ JavaToolBox/src/Main.java
@@ -1,3 +1,15 @@
+/*
+ * 	Copyright (c) 2017 IN2C
+ *	All Rights Reserved
+ *
+ *	This product is protected by copyright and distributed under
+ *  licenses restricting copying, distribution and decompilation.
+ *  
+ *  GitHubUrl: https://github.com/SilasNaef1/JavaToolBox.git
+ *  Date: 13.03.1017
+ *  LastChange: Jannis
+ *  Author: Silas
+ */
 
 public class Main {
 

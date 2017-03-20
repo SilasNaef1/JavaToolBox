880160a58741b22a7d3d9a0d3e3a9af79da64b5a
SilasNaef1 <silas.naef@griesser.ch>
2017-03-20 10:30:06 +0100

    Round Method

---
+++ src/mathFormulars/Round.java
@@ -0,0 +1,19 @@
+/*
+ * 	Copyright (c) 2017 IN2C
+ *	All Rights Reserved
+ *
+ *	This product is protected by copyright and distributed under
+ *  licenses restricting copying, distribution and decompilation.
+ *  
+ *  GitHubUrl: https://github.com/SilasNaef1/JavaToolBox.git
+ *  Date: 20.03.2017
+ *  LastChange: muster
+ *  Author muster
+ */
+package mathFormulars;
+
+public class Round {
+	private double startValue;
+	private double result;
+	public void RoundValue(double value) {
+}

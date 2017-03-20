e3161e5d464276ce8df506b7a337dacaa232b323
SilasNaef1 <silas.naef@griesser.ch>
2017-03-20 16:13:06 +0100

    asdf

--- src/mathFormulars/Round.java
+++ src/mathFormulars/Round.java
@@ -12,23 +12,45 @@
  */
 package mathFormulars;
 
+//Kann Zahlen von 0 bis auf 4 stellen Runden.
 public class Round {
 	private static double startValue;
 	private static int decimalDigits;
+	private static int Multiplier;
 	private static double inCalc;
 	private static double result;
 	//Diese Methode wird von ausserhalb der Klasse aufgerufen
 	public static double RoundValue(double value, int decimaldigits) {
 		startValue = value;
 		decimalDigits = decimaldigits;
+		getDecimalDigitMultiplier(decimalDigits);
 		Calc(value);
 		return result;
 	}
 	//Diese Methode wird innerhalb der Klasse aufgerufen um zu Runden.
 	private static void Calc(double value) {
-		inCalc = value * 100;
+		inCalc = value * Multiplier;
 		inCalc = Math.round(inCalc);
-		value = inCalc / 100;
+		value = inCalc / Multiplier;
 		result = value;
+		
+	}
+	//Setzt den multiplier um die Zahl richtig zu runden.
+	private static void getDecimalDigitMultiplier(int digits) {
+		int multiplier = 0;
+		switch(digits) {
+			case 1:	multiplier = 10;
+				break;
+			case 2:	multiplier = 100;
+				break;
+			case 3:	multiplier = 1000;
+				break;
+			case 4:	multiplier = 10000;
+				break;
+			case 0: multiplier = 1;
+				break;
+			default: multiplier = 1;
+		}
+		Multiplier = multiplier;
 	}
 }

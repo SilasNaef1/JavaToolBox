2ff98217c5186cccd93466586deb07fd2a3121c2
ChrisBuch <chbuchmannch@gmail.com>
2017-03-27 15:57:46 +0200

    asdfasfd

--- src/Main.java
+++ src/Main.java
@@ -28,45 +28,17 @@ public class Main {
 	public static void Test()
 	{
 		Function test = mathFormulars.Function.linearFunction(6, -1, 2, 2);
-
+		
 	
-		System.out.println("Testfälle Dreieck!");
-		System.out.println("______________________________________________");
 		
-		
-		System.out.println("Testfall1 Dreieck!");
+		System.out.println("Testfall Rechtwinkliges Dreieck");
 		System.out.println("************************************");
-		
-		System.out.println(RightTriangle.calcArea(12.54, 21.25));	//Erwartet 133.238Berechnet Fläche vom Dreieck
-		System.out.println(RightTriangle.calcCorner(30.546));		//Erwartet 59.454Berechnet den fehlenden Winkel
-		System.out.println(RightTriangle.calcHeight(12.54, 21.25, 24.674));		//Erwartet 10.8Berechnet die Höhe vom Dreieck WENN NICHT RECHTWINKLIG IST DAS RESULTAT 0!
-		System.out.println(RightTriangle.calcHypotenuse(12.54, 21.25));		//Erwartet 24.674Berechnet die Hypotenuse vom Dreieck
-		System.out.println(RightTriangle.calcScope(12.54, 21.25, 24.674));		//Berechnet den Umfang vom Dreieck WENN NICHT RECHTWINKLIG IST DAS RESULTAT 0!
-		System.out.println(RightTriangle.calcKathete(12.54, 21.25));		//Erwartet 21.25Berechnet die fehlende Kathete vom Dreieck
-		
-		System.out.println("Testfall2 Dreieck!");
-		System.out.println("************************************");		
-		System.out.println(RightTriangle.calcArea(3, 4));			//Erwartet 6 Berechnet Fläche vom Dreieck
-		System.out.println(RightTriangle.calcCorner(53.13));			//Erwartet 36.87 Berechnet den fehlenden Winkel
-		System.out.println(RightTriangle.calcHeight(3, 4, 5));		//Erwartet 2.4 Berechnet die Höhe vom Dreieck WENN NICHT RECHTWINKLIG IST DAS RESULTAT 0!
-		System.out.println(RightTriangle.calcHypotenuse(3, 4));		//Erwartet 5 Berechnet die Hypotenuse vom Dreieck
-		System.out.println(RightTriangle.calcScope(3, 4, 5));		//Erwartet 12 Berechnet den Umfang vom Dreieck WENN NICHT RECHTWINKLIG IST DAS RESULTAT 0!
-		System.out.println(RightTriangle.calcKathete(3, 5));		    //Erwartet 4 Berechnet die fehlende Kathete vom Dreieck
-		
-		
-		System.out.println("Testfall3 Dreieck!");
-		System.out.println("************************************");		
-		System.out.println(RightTriangle.calcArea(3, 4));			//Erwartet 6 Berechnet Fläche vom Dreieck
-		System.out.println(RightTriangle.calcCorner(180));			//Erwartet 36.87 Berechnet den fehlenden Winkel
-		System.out.println(RightTriangle.calcHeight(3, 4, 5));		//Erwartet 2.4 Berechnet die Höhe vom Dreieck WENN NICHT RECHTWINKLIG IST DAS RESULTAT 0!
-		System.out.println(RightTriangle.calcHypotenuse(3, 4));		//Erwartet 5 Berechnet die Hypotenuse vom Dreieck
-		System.out.println(RightTriangle.calcScope(3, 4, 5));		//Erwartet 12 Berechnet den Umfang vom Dreieck WENN NICHT RECHTWINKLIG IST DAS RESULTAT 0!
-		System.out.println(RightTriangle.calcKathete(3, 5));		    //Erwartet 4 Berechnet die fehlende Kathete vom Dreieck
-		System.out.println("______________________________________________");
-
-		System.out.println("Ende Testfälle Dreieck!");
-		
-		
+		if(RightTriangle.test()) {
+			System.out.println("RightTriangle: " + "Passed!");
+		} else {
+			System.out.println("RightTriangle: " + "Failed!");
+		}
+	
 		System.out.println("Testfall StringToInt");
 		System.out.println("************************************");
 		if(StringToInt.Test()) {

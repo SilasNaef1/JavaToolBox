457d9a8b3a8081197598164acf2cae015c81bf37
JannisThymnios <j.thymnios@gmail.com>
2017-03-27 10:17:45 +0200

    quersumme

--- src/mathFormulars/QuerSumme.java
+++ src/mathFormulars/QuerSumme.java
@@ -13,5 +13,25 @@
 package mathFormulars;
 
 public class QuerSumme {
+	
+	static int QuersummeInt(int zahl) {
+	    // Die zahl wird solange durch ihre Quersumme
+	    // ersetzt, bis das Ergebnis einstellig ist
+
+	    // Iterieren der Quersummenbildung:
+	    while (zahl > 9) {
+	      
+	      // Berechnung der einfachen Quersumme:
+	      int quersumme = 0;
+	      do {        
+	        quersumme = quersumme + zahl % 10;
+	        zahl = zahl / 10;
+	      } while (zahl > 0);
+
+	      zahl = quersumme;
+	    }
+
+	    return zahl;
+	  }
 
 }
